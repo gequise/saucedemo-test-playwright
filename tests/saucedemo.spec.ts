f1d947057eef0aeff29eeaa3c53bea684597c74c
Gustavo Equise <gustavo.equise@gmail.com>
2023-07-05 15:06:29 -0300

    fix: assert test

--- tests/saucedemo.spec.ts
+++ tests/saucedemo.spec.ts
@@ -4,8 +4,10 @@ import { HomePage } from "../pages/HomePage";
 test.describe("Login Success", () => {
   let homePage: HomePage;
   test.beforeEach(async ({ page }: { page: Page }) => {
-    homePage = new HomePage(page);
-    await homePage.urlHomePage();
+    await test.step(`I can enter the login Page`, async () => {
+      homePage = new HomePage(page);
+      await homePage.urlHomePage();
+    });
   });
 
   test("login with standard_user", async ({ page }: { page: Page }) => {
@@ -13,6 +15,6 @@ test.describe("Login Success", () => {
     const currentUrl = page.url();
     expect(currentUrl).toBe("https://www.saucedemo.com/inventory.html");
     const pageTitle = await page.title();
-    expect(pageTitle).toBe("Swag Lab");
+    expect(pageTitle).toBe("Swag Labs");
   });
 });
